16ffc8af11a4e911eb1b6d6da70c7d60e2f18259
Wee <34083048+eewartlu@users.noreply.github.com>
2019-06-27 13:47:09 +0700

    Update nvidia-query.php

--- nvidia-query.php
+++ nvidia-query.php
@@ -1,4 +1,26 @@
 <?php
-// pretty soon
+// comment/uncomment the value that you want or not want to see
+// please check the query code and explanation in our Wiki
+
+$queries = array(
+	'index',
+	'temperature.gpu',
+	'power.min_limit',
+	'power.max_limit',
+	'power.default_limit',
+	'utilization.gpu',
+	'utilization.memory',
+//	'memory.total',
+//	'memory.used',
+//	'memory.free',
+	'fan.speed',
+	'pstate',
+	'power.draw',
+	'power.limit',
+	'clocks.gr',
+	'clocks.mem',
+	'clocks.max.gr',
+	'clocks.max.mem',
+);
 
 ?>
